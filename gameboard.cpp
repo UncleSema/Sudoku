0d1412b1c0eb2a96492221b2dc833859f9c30314
UncleSema <semenkorobko2@gmail.com>
2021-08-08 13:56:36 +0500

    Added c++ files

---
+++ gameboard.cpp
@@ -0,0 +1,98 @@
+#include "gameboard.h"
+
+GameBoard::GameBoard(QObject *parent)
+{
+    Q_UNUSED(parent);
+    srand(clock());
+    m_board.resize(dimensions, std::vector<size_t>(dimensions));
+    fillBoard(baseDifficulty);
+}
+
+void GameBoard::fillBoard(const size_t &difficulty)
+{
+    // Try to open the file
+    const std::string fileName = "sudoku/"+std::to_string(difficulty) + ".txt";
+    QFile file(fileName.c_str());
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        throw 1;
+    }
+
+    // Read from file
+    QTextStream in(&file);
+    std::vector<std::string> lines;
+    while (!in.atEnd()) {
+        lines.push_back(in.readLine().toStdString());
+    }
+
+    // Fill our board with the read value
+    size_t linesNumber = lines.size();
+    std::string board = lines[rand()%linesNumber];
+    for (size_t index = 0; index<dimensions*dimensions; index++) {
+        size_t x = getX(index), y = getY(index);
+        m_board[x][y] = board[index]-'0';
+    }
+    m_defaultBoard = m_board;
+}
+
+size_t GameBoard::getValue(const size_t &index)
+{
+    size_t x = getX(index), y = getY(index);
+    return m_board[x][y];
+}
+
+void GameBoard::setValue(const size_t &index, const size_t &newValue)
+{
+    size_t x = getX(index), y = getY(index);
+    m_board[x][y] = newValue;
+}
+
+bool GameBoard::isValueCorrect(const size_t &index)
+{
+    size_t x = getX(index), y = getY(index);
+    if (m_board[x][y] == 0) return true;
+
+    //Check whether there is the same value in the same raw/column
+    for (size_t i = 0; i<dimensions; i++) {
+        if ((y != i && m_board[x][y] == m_board[x][i]) || (i != x && m_board[i][y] == m_board[x][y])) {
+            return false;
+        }
+    }
+
+    //Check whether there is the same value in the same square
+    size_t x_square = x/3, y_square = y/3;
+    const size_t leftSquareBoundX = x_square*3, leftSquareBoundY = y_square*3;
+    const size_t rightSquareBoundX = (x_square+1)*3, rightSquareBoundY = (y_square+1)*3;
+    for (size_t i = leftSquareBoundX; i<rightSquareBoundX; i++) {
+        for (size_t j = leftSquareBoundY; j<rightSquareBoundY; j++) {
+            if (m_board[i][j] == m_board[x][y] && (i!=x || j != y)) return false;
+        }
+    }
+    return true;
+}
+
+const QString GameBoard::getColor(const size_t &index)
+{
+    if (!isValueCorrect(index)) return wrongTileColor;
+    if (isValueDefault(index)) return defaultTileColor;
+    return newTileColor;
+}
+
+bool GameBoard::isValueDefault(const size_t &index)
+{
+    size_t x = getX(index), y = getY(index);
+    return static_cast<bool>(m_defaultBoard[x][y]);
+}
+
+size_t GameBoard::getX(const size_t &index)
+{
+    return index/dimensions;
+}
+
+size_t GameBoard::getY(const size_t &index)
+{
+    return index%dimensions;
+}
+
+constexpr size_t GameBoard::getDimensions() {
+    return dimensions;
+}
---
+++ gameboard.h
@@ -0,0 +1,38 @@
+#ifndef GAMEBOARD_H
+#define GAMEBOARD_H
+
+#include <QObject>
+#include <vector>
+#include <QFile>
+#include <QTextStream>
+
+class GameBoard : public QObject
+{
+    Q_OBJECT
+public:
+    GameBoard(QObject *parent = nullptr);
+
+    Q_INVOKABLE size_t getValue(const size_t &index);
+    Q_INVOKABLE const QString getColor(const size_t &index);
+    Q_INVOKABLE void setValue(const size_t &index, const size_t &newValue);
+    Q_INVOKABLE bool isValueCorrect(const size_t &index);
+    Q_INVOKABLE bool isValueDefault(const size_t &index);
+    Q_INVOKABLE static constexpr size_t getDimensions();
+
+private:
+    std::vector<std::vector<size_t>> m_board;
+    std::vector<std::vector<size_t>> m_defaultBoard;
+
+    static constexpr size_t dimensions = 9;
+    static constexpr size_t baseDifficulty = 3;
+
+    const QString wrongTileColor = "red";
+    const QString defaultTileColor = "black";
+    const QString newTileColor = "#808080";
+
+    void fillBoard(const size_t &difficulty);
+    size_t getX(const size_t &index);
+    size_t getY(const size_t &index);
+};
+
+#endif // GAMEBOARD_H
